eaea53f52218a13684213cb25c0cd1481d8cc454
hana <hanaabrham89@gmail.com>
2025-08-15 14:10:56 +0200

    refactor: Reformat Transaction type for improved readability

--- src/types/transactions.ts
+++ src/types/transactions.ts
@@ -1,8 +1,13 @@
-  export type Transaction = {
-    Kategorie: "Gehalt" | "Miete" | "Investitionen" | "Lebensmittel" | "Shopping" | "Freizeit" | "Sonstiges";
-    amount: number;
-    Transaktionstyp: "income" | "expense";
-    date?: string;
-  };
-
-
+export type Transaction = {
+  Kategorie:
+    | "Gehalt"
+    | "Miete"
+    | "Investitionen"
+    | "Lebensmittel"
+    | "Shopping"
+    | "Freizeit"
+    | "Sonstiges";
+  amount: number;
+  Transaktionstyp: "income" | "expense";
+  date?: string;
+};
